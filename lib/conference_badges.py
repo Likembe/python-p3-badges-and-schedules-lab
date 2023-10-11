20af51e3f3856328a845aa68c56fa09488ef8e17
Alpha <alpha.likembe@student.moringaschool.com>
2023-10-11 03:24:58 +0300

    Complete

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,26 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+
+    for name in names:
+        badges.append(f"Hello, my name is {name}.") 
+    return badges
 
 def assign_rooms(names):
-    return None
+    welcome_messages = []
+
+    for i, name in enumerate(names, start=1):
+        welcome_messages.append(f"Hello, {name}! You'll be assigned to room {i}!")
+    return welcome_messages
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    welcome_messages = assign_rooms(names)
+
+    for badge in badges:
+        print(badge)
+
+    for message in welcome_messages:
+        print(message)
